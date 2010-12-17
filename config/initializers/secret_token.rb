2b173a5dcbda21b5c4356ca6ce6b997ba8455221
migueldiab <migueldiab@gmail.com>
2010-12-17 17:22:44 -0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+JMilla::Application.config.secret_token = '01b02bcb6e5ace882076c017e2c291bb4f95d83a8b2dc7d2c1b93b7fef928c882acde6eff7a2e12be32126aa18668eeb03953ff9c451efad7d39899b9b245f69'
